b597140d1d39f06403ab6ac69094772ad1e54080
Dani Lyn <dlyn1110@gmail.com>
2018-04-11 13:44:44 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,8 +2,12 @@
 
 def my_each(array) # put argument(s) here
   counter = 0
+  empty_array = []
+
   my_each(words) do |word|
   while counter < array.length
     yield(words[counter])
     counter +=1
+  end
+  array
 end
