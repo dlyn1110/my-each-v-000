561eb1f05c2cef1dabe5f039fc0e703f8043aba8
Dani Lyn <dlyn1110@gmail.com>
2018-04-11 13:37:18 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,9 @@
-require 'pry'
+#require 'pry'
 
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+  counter = 0
+  my_each(words) do |n|
+  while n 
+    yield(words[counter])
+    counter +=1
 end
