a5aa3303d57f66c6feb670d3990fb40e815ac670
Dani Lyn <dlyn1110@gmail.com>
2018-04-11 14:00:02 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -3,11 +3,12 @@ require 'pry'
 def my_each(array) # put argument(s) here
   counter = 0
   empty_array = []
-binding.pry
+#binding.pry
   #my_each(words) do |word|
   while counter < array.length
     yield(words[counter])
     counter +=1
+    binding.pry
   end
   array
  end
